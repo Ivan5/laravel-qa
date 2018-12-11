819c215d6a30e8f6c02031888e1af68791ebcd29
Ivan5 <converse_ivan@hotmail.com>
2018-12-11 16:16:16 -0600

    Add votes, answers and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -22,7 +22,18 @@ class Question extends Model
         return route("questions.show", $this->slug);
     }
 
-     public function getCreatedDateAttribute(){
+    public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute(){
+        if($this->answers > 0){
+            if($this->best_answer_id){
+                return "answer-accepted";
+            }
+            return "answered";
+        }else{
+            return "unanswered";
+        }
     }
 }
