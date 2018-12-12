1cee26cf54a242612617ff7156c525f8281f5474
Ivan5 <converse_ivan@hotmail.com>
2018-12-11 23:21:28 -0600

    Implement show functionality for the questions

--- app/Question.php
+++ app/Question.php
@@ -36,4 +36,9 @@ class Question extends Model
             return "unanswered";
         }
     }
+
+    public function getBodyHtmlAttribute()
+    {
+        return \Parsedown::instance()->text($this->body);
+    }
 }
