d1d8d854930eedbfe8de6aee7fe83457e3d713c6
Ivan5 <converse_ivan@hotmail.com>
2018-12-18 21:24:48 -0600

    Update bast_answer_id on questions table when an answer remove using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -16,6 +16,10 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -35,7 +39,12 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if ($question->best_answer_id === $answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 }
